f672188f806a13cce77056e7872fadf80a4367c8
steven <steven@macbook-pro.local>
2021-11-06 12:01:08 +0000

    Refactor app with various fixes
    No longer requires hard coded boards. Boards are generated from the board_size.
    Final board the program tests against is still generated from a final string.

--- app.py
+++ app.py
@@ -1,67 +1,50 @@
 import time
 
-board = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0]
-]
-
-solved_board = [
-    [0, 0, 1, 0, 0],
-    [0, 1, 1, 1, 0],
-    [1, 1, 1, 1, 1],
-    [0, 1, 1, 1, 0],
-    [0, 0, 1, 0, 0]
-]
-
-hints = [
-    [1, 3, 5, 3, 1],
-    [1, 3, 5, 3, 1]
-]
-
 
 class NonogramSolver(object):
+
+
     def __init__(self, board_size, hints):
-        board = [[0*board_size]*board_size]
+        self.board_size = board_size
+        self.board = [ [0] * board_size ] * board_size
+        self.hints = hints
 
 
-def generate_board(board):
-    return [
-        [int(char) for char in board[0:5]],
-        [int(char) for char in board[5:10]],
-        [int(char) for char in board[10:15]],
-        [int(char) for char in board[15:20]],
-        [int(char) for char in board[20:25]],
-    ]
+    def generate_board(self, board):
+        my_list = [int(char) for char in [row for row in board]]
+        return [my_list[i * self.board_size:(i + 1) * self.board_size] for i in range((len(my_list) + self.board_size - 1) // self.board_size )]
 
 
-def check(potential_solve):
-    potential_board = generate_board(potential_solve)
-    if potential_board != solved_board:
+    def check(self, potential_solve):
+        potential_board = self.generate_board(potential_solve)
+        if potential_board == self.generate_board("0010001110111110111000100"):
+            print("Solved")
+            return True
         return False
-    return True
 
 
-def solve():
-    stack = []
-    stack.append("0")
-    stack.append("1")
-    while stack:
-        current = stack.pop()
-        if len(current) == 25:
-            if check(current):
-                print("Success")
-                solved_board = generate_board(current)
-                for row in solved_board:
-                    print(row)
-                break
-        else:
-            stack.append(current + "0")
-            stack.append(current + "1")
+    def solve(self):
+        self.stack = []
+        self.stack.append("0")
+        self.stack.append("1")
+        while self.stack:
+            current = self.stack.pop()
+            if len(current) == self.board_size * self.board_size:
+                if self.check(current):
+                    print("Success")
+                    solved_board = self.generate_board(current)
+                    for row in solved_board:
+                        print(row)
+                    break
+            else:
+                self.stack.append(current + "0")
+                self.stack.append(current + "1")
+
+
+nono = NonogramSolver(board_size=5, hints=[[1,3,5,3,1],[1,3,5,3,1]])
 
 start = time.time()
-solve()
+nono.solve()
 end = time.time()
+
 print(end - start)
