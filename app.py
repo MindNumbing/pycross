1137190c8e0cf231ca6bee906993044572d9a51f
steven <steven@macbook-pro.local>
2021-11-06 11:47:50 +0000

    Adds initial solver
    Completes search for 5x5 board in 2:45 seconds.
    Adds tqdm to visualise the progress of the solve.
    -  Look into how much performance this is costing

---
+++ app.py
@@ -0,0 +1,68 @@
+from tqdm import tqdm
+
+pbar = tqdm(total=33554432)
+
+board = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+]
+
+solved_board = [
+    [0, 0, 1, 0, 0],
+    [0, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 0],
+    [0, 0, 1, 0, 0]
+]
+
+hints = [
+    [1, 3, 5, 3, 1],
+    [1, 3, 5, 3, 1]
+]
+
+
+class NonogramSolver(object):
+    def __init__(self, board_size, hints):
+        board = [[0*board_size]*board_size]
+
+
+def generate_board(board):
+    return [
+        [int(char) for char in board[0:5]],
+        [int(char) for char in board[5:10]],
+        [int(char) for char in board[10:15]],
+        [int(char) for char in board[15:20]],
+        [int(char) for char in board[20:25]],
+    ]
+
+
+def check(potential_solve):
+    potential_board = generate_board(potential_solve)
+    if potential_board != solved_board:
+        return False
+    return True
+
+
+def solve():
+    stack = []
+    stack.append("0")
+    stack.append("1")
+    while stack:
+        current = stack.pop()
+        if len(current) == 25:
+            pbar.update(1)
+            if check(current):
+                print("Success")
+                solved_board = generate_board(current)
+                for row in solved_board:
+                    print(row)
+                break
+        else:
+            stack.append(current + "0")
+            stack.append(current + "1")
+
+solve()
+pbar.close()
